b3b8cd5d8073c319edf38f8079dea11cfc87def8
SU15VTE <110088179+SU15VTE@users.noreply.github.com>
2023-04-07 11:53:24 +0800

    Update root.go

--- cmd/root.go
+++ cmd/root.go
@@ -12,8 +12,8 @@ var rootCmd = &cobra.Command{
 	Use:   "des",
 	Short: "A brief description of your application",
 	Long: `GO_DES is a program for DES encryption and decryption. You can use it like this:
-	.\des.exe encrypt --key "SU15VTE!" --text "Hello World!"
-	.\des.exe decrypt --key "SU15VTE!" --text "1c3568382f24163e7368342104541010".`,
+	.\godes.exe encrypt --key "SU15VTE!" --text "Hello World!"
+	.\godes.exe decrypt --key "SU15VTE!" --text "1c3568382f24163e7368342104541010".`,
 }
 
 func Execute() {
